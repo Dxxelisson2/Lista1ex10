2cd6a674431149b1ed3d8843d281c4e6384fc625
Dxxelisson2 <130613973+Dxxelisson2@users.noreply.github.com>
2023-04-12 21:14:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista1_ex_10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cot, quantidade, reais;
+            Console.WriteLine("Digite a quantidade de dolares");
+            quantidade = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor da cotação:");
+            cot = Convert.ToDouble(Console.ReadLine());
+
+            reais = quantidade * cot;
+
+            Console.WriteLine("o valor em reais é: R$" + reais);
+
+            Console.ReadKey();
+        }
+    }
+}
